b456dbf13429f8f1b79e068ebedbdf9f8a007725
Leonardo Araujo <leocrazy007@hotmail.com>
2020-02-17 22:38:35 -0300

    criacao de componentes e estilizacao

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,45 @@
 import React from 'react';
+import './global.css';
+import './App.css';
+import './Sidebar.css';
 
 function App() {
+
+
   return (
-      <h1>Ola Mundo</h1>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuario do GitHub</label>
+            <input name="github_username" id="username_github" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+          
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+
+      </main>
+    </div>
   );
 }
 
